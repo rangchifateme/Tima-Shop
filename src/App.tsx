8cbe62f66e7bb09f725c88b2f9e31b7ede986647
rangchifateme <fatima.rangchi@quartech.com>
2024-11-20 11:36:36 -0800

    initial commit with typscript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App with TypeScript</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
