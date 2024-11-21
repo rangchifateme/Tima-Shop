e655113337e503d060858a59e42283de22809526
rangchifateme <fatima.rangchi@quartech.com>
2024-11-20 16:59:43 -0800

    next framework added to the package

---
+++ src/Main.tsx
@@ -0,0 +1,41 @@
+// filepath: /c:/Users/RangchiJafarrangchiF/Desktop/Copilot%20app/src/Main.tsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+const Main: React.FC = () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default Main;
